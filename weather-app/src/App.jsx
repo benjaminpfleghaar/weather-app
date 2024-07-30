5e124e3cfe83c2cfe2d5d90b37a92dde5597c0e1
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-07-30 14:38:11 +0200

    Implement useEffect() Function

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,32 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "./Input";
 import Weather from "./Weather";
 
 export default function App() {
+	const [city, setCity] = useState("");
+	const [isFetchingData, setIsFetchingData] = useState(false);
 	const [locationWeather, setLocationWeather] = useState({});
 
-	const getWeatherData = async (city) => {
-		// Get coordinates based on city name from https://nominatim.org/
-		const coordinatesRessource = await fetch(
-			`https://nominatim.openstreetmap.org/search?q=${city}&format=jsonv2&limit=1&accept-language=en-US`
-		);
-		const coordinatesData = await coordinatesRessource.json();
+	useEffect(() => {
+		const fetchDataAsync = async (city) => {
+			if (isFetchingData) {
+				// Get coordinates based on city name from https://nominatim.org/
+				const coordinatesRessource = await fetch(
+					`https://nominatim.openstreetmap.org/search?q=${city}&format=jsonv2&limit=1&accept-language=en-US`
+				);
+				const coordinatesData = await coordinatesRessource.json();
 
-		// Pass coordinates to get weather data from https://open-meteo.com/
-		const weatherRessource = await fetch(
-			`https://api.open-meteo.com/v1/forecast?latitude=${coordinatesData[0].lat}&longitude=${coordinatesData[0].lon}&current=temperature_2m`
-		);
-		const weatherData = await weatherRessource.json();
+				// Pass coordinates to get weather data from https://open-meteo.com/
+				const weatherRessource = await fetch(
+					`https://api.open-meteo.com/v1/forecast?latitude=${coordinatesData[0].lat}&longitude=${coordinatesData[0].lon}&current=temperature_2m`
+				);
+				const weatherData = await weatherRessource.json();
 
-		setLocationWeather({
-			city: coordinatesData[0].display_name,
-			temperature: weatherData.current.temperature_2m + "°",
-		});
-	};
+				setLocationWeather({
+					city: coordinatesData[0].display_name,
+					temperature: weatherData.current.temperature_2m + "°",
+				});
+			}
+		};
+
+		fetchDataAsync(city);
+
+		return () => {
+			setIsFetchingData(false);
+			setCity("");
+		};
+	}, [isFetchingData]);
 
 	return (
 		<>
-			<Input getWeatherData={getWeatherData} />
+			<Input
+				setIsFetchingData={setIsFetchingData}
+				city={city}
+				setCity={setCity}
+			/>
 			<Weather locationWeather={locationWeather} />
 		</>
 	);
