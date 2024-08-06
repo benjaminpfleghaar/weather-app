890866fd461fe0c7003a2dc66685c4f201c8efae
Benjamin Pfleghaar <info@benjaminpfleghaar.de>
2024-08-06 09:19:09 +0200

    Refactor cloud cover calculation

--- weather-app/src/helpers/Utils.js
+++ weather-app/src/helpers/Utils.js
@@ -1,12 +1,12 @@
 // Calculate cloud cover (0 - 100)
 export const calculateCloudCover = (cloudCover) => {
-	if (cloudCover >= 0 && cloudCover <= 25) {
-		return "☀️";
-	} else if (cloudCover > 25 && cloudCover <= 50) {
-		return " 🌤️";
-	} else if (cloudCover > 50 && cloudCover <= 75) {
-		return "⛅️";
-	} else {
+	if (cloudCover >= 75) {
 		return "☁️";
+	} else if (cloudCover >= 50) {
+		return "⛅️";
+	} else if (cloudCover >= 25) {
+		return " 🌤️";
+	} else {
+		return "☀️";
 	}
 };
